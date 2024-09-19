1722c89b44082c3a1aeee9f14af7b17db09f289c
Kuixz <u.quixz@gmail.com>
2024-09-19 23:10:41 +0800

    Fix return types in SandShelf

--- src/content/foundation/Shelf.ts
+++ src/content/foundation/Shelf.ts
@@ -16,37 +16,42 @@ class SandShelf implements IShelf { // FAKE SHELF - REMOVE THIS WHEN PUSHING BET
         for (const key in items) {
             this.dict[key] = items[key]
         }
+        return Promise.resolve()
     }
-    async get(items: string|string[]|any): Promise<{[key: string]: any}> { // [String]
+    async get(items: string|string[]): Promise<{[key: string]: any}> { // [String]
         if (typeof items == "string") {
-            return this.dict[items]
+            var t: {[key: string]: any} = {}
+            t[items] = this.dict[items]
+            return Promise.resolve(t)
         }
         if (Array.isArray(items)) {
             var t: {[key: string]: any} = {}
             Object.entries(items).forEach(([key, value]: [string, any]) => {
                 t[key] = value
             })
-            return t
+            return Promise.resolve(t)
         }
-        return this.dict
+        return Promise.resolve(this.dict)
     }
     async remove(items: string|string[]): Promise<void> { // [String]
         if (typeof items == "string") {
-            return this.dict[items]
+            delete this.dict[items]
         }
         if (Array.isArray(items)) {
             for (const key in items) {
                 delete this.dict[key]
             }
         }
+        return Promise.resolve()
     }
     async clear(): Promise<void> {
         this.dict = { }
+        return Promise.resolve()
     }
     async retrieveOrElse(key: string, defaultValue: any, write: boolean = false): Promise<any> {
         if (key in this.dict) { return this.dict[key] }
         if (write) { this.dict[key] = defaultValue }
-        return defaultValue
+        return Promise.resolve(defaultValue)
     }
 }
 const LiveShelf = {
@@ -62,7 +67,7 @@ const LiveShelf = {
     async set(items: {[key: string]: any}): Promise<void> { // Dictionary<String, any>
         return await chrome.storage.local.set(items)
     },
-    async get(items: {[key: string]: any}): Promise<{[key: string]: any}> { // [String]
+    async get(items: string|string[]): Promise<{[key: string]: any}> { // [String]
         return await chrome.storage.local.get(items)
     },
     async remove(items: string|string[]): Promise<void> { // [String]
